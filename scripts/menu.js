0333c59f7369eac86589346a8c5f7d6e9f4785f7
Omar Sherif <0marsherif.pro85@gmail.com>
2025-09-25 12:53:56 +0300

    update resposive design and fix menu toggle issue

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const icon = document.querySelector(".menu");
-    const header = document.querySelector(".navbar span");
+    const icon = document.querySelector(".menubar");
+    const navMenu = document.querySelector(".navbar");
 
     icon.addEventListener("click", function (event) {
         event.stopPropagation();
-        header.classList.toggle("open");
+        navMenu.classList.toggle("open");
         icon.classList.toggle("open");
     });
 
     document.addEventListener("click", function (event) {
-        if (header.classList.contains("open") && !header.contains(event.target) && event.target !== icon) {
-            header.classList.remove("open");
+        if (navMenu.classList.contains("open") && !navMenu.contains(event.target) && event.target !== icon) {
+            navMenu.classList.remove("open");
             icon.classList.remove("open");
         }
     });
 
     window.addEventListener("scroll", function () {
-        if (header.classList.contains("open")) {
+        if (navMenu.classList.contains("open")) {
             navMenu.classList.remove("open");
             icon.classList.remove("open");
         }
