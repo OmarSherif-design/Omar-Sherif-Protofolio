5be8bbf89c2f6a8451627bb0aaae72805046c66b
Omar Sherif <0marsherif.pro85@gmail.com>
2025-11-21 15:54:04 +0200

    remove # from URL

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,32 +1,23 @@
-// Disable scroll at start
 document.documentElement.classList.add("no-scroll");
 
 window.addEventListener("load", () => {
   const loading = document.getElementById("loading");
   const header = document.querySelector("header");
 
-  // Get the CSS background image URL
   const bgImage = getComputedStyle(document.querySelector('.about-photo .img')).backgroundImage;
 
-  // Extract URL from url("...")
   const imageUrl = bgImage.slice(5, -2);
 
-  // Preload the background image
   const img = new Image();
   img.src = imageUrl;
 
   img.onload = () => {
-    // Image loaded → hide loader
     loading.style.opacity = "0";
 
     setTimeout(() => {
       loading.style.display = "none";
-
-      // Show header
       header.style.opacity = "1";
       header.style.pointerEvents = "auto";
-
-      // Enable scrolling again
       document.documentElement.classList.remove("no-scroll");
 
     }, 700);
